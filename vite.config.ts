57df2a7a7a3c09743f9ea807ad5dfa1cd6fbc1ee
Redth <redth1206@proton.me>
2024-02-06 13:16:19 +0800

    fix script

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import topLevelAwait from "vite-plugin-top-level-await";
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     topLevelAwait(),
-    viteSingleFile(),
   ],
   build: {
     assetsDir: ".",
